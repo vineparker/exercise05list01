7828560093ff70a5f0a1755d50f9d81fa6db3d1f
vineparker <viniciusalves.5262@aluno.una.br>
2023-04-18 02:02:00 -0300

    Atividade 5 ver 1

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main {
+  // Faça um Programa que converta metros para centímetros.
+  public static void main(String[] args) {
+
+    // declaração das variáveis 
+    Scanner input = new Scanner(System.in);double nresult; 
+    double nmetro;
+    double ncent;
+
+    // primeira mensagem
+    String textoUm = "Informe um valor em metros (m) ";
+    System.out.print(textoUm);
+    nmetro = input.nextDouble();
+    ncent = 100.00;
+    // fazer o calculo de conversao de medidas m x cm
+    
+    nresult = (nmetro * ncent);
+
+    // mostrar o resultado da soma
+    System.out.println("O Valor digitado em centimetros equivale a: " + nresult);
+
+    input.close();
+  }
+}
+
+
+
+
+
